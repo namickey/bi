55d5bd7b4188edc82847110269fb0e321e82555e
namiki <namiki.daisuke.366@tis.co.jp>
2018-07-23 13:04:45 +0900

    fix

--- 3.graph.cus/app.py
+++ 3.graph.cus/app.py
@@ -5,23 +5,66 @@ import pygal
 
 app = Flask(__name__)
 
-@app.route('/t')
-def graph_tis():
+@app.route('/f')
+def graph_for():
+    return '<html><body><div style="width:800px">%s</div></body></html>' % getFor().render()
+
+def getFor():
     #顧客別
     df = pd.read_csv('upyear.csv',encoding='utf-8')
     g = df.groupby('cus')
     t = g.sum().sort_values('for', ascending=False)['for']
     chart = pygal.Bar(x_label_rotation=40)
+    chart.title = u'2018年度：年間売上計画（顧客別）'
     chart.x_labels = t.index
     chart.add(u'売上', t.values)
-    return '<html><body><div style="width:1100px">%s</div></body></html>' % chart.render()
+    return chart
+
+@app.route('/a')
+def graph_ac():
+    return '<html><body><div style="width:800px">%s</div></body></html>' % getAc().render()
+
+def getAc():
+    #顧客別
+    df = pd.read_csv('upyear.csv',encoding='utf-8')
+    g = df.groupby('cus')
+    t = g.sum().sort_values('ac', ascending=False)['ac']
+    chart = pygal.Bar(x_label_rotation=40)
+    chart.title = u'2018年度：年間売上実績（顧客別）'
+    chart.x_labels = t.index
+    chart.add(u'売上', t.values)
+    return chart
 
 @app.route('/')
 def graph():
+    return '<html><body><div style="width:800px">%s</div></body></html>' % gethist().render()
+
+def gethist():
     #売上見込みのヒストグラム
     df = pd.read_csv('upyear.csv')
     a = df['for'].astype(int)/1000
     chart = pygal.Bar()
-    chart.x_labels = range(0,65,5)
-    chart.add('up', [((r<=a) & (a<r+5)).sum() for r in range(0,65,5)])
-    return '<html><body><div style="width:1100px">%s</div></body></html>' % chart.render()
+    chart.title = u'2018年度：年間案件数（売上別）'
+    chart.x_labels = range(0,65000,5000)
+    chart.add(u'案件数', [((r<=a) & (a<r+5)).sum() for r in range(0,65,5)])
+    return chart
+
+def gethist_t():
+    #売上見込みのヒストグラム
+    df = pd.read_csv('upyear.csv')
+    a = df.loc[df['cus'] == 'T' + 'I' + 'S']['for'].astype(int)/1000
+    chart = pygal.Bar()
+    chart.title = u'2018年度：年間案件数（売上別）'
+    chart.x_labels = range(0,65000,5000)
+    chart.add(u'案件数', [((r<=a) & (a<r+5)).sum() for r in range(0,65,5)])
+    return chart
+
+@app.route('/all')
+def graph_all():
+    ret = '<html><body>'
+    ret += '<div style="width:800px">%s</div>' % gethist().render()
+    ret += '<div style="width:800px">%s</div>' % gethist_t().render()
+    ret += '<div style="width:800px">%s</div>' % getAc().render()
+    ret += '<div style="width:800px">%s</div>' % getFor().render()
+    ret += '</body></html>'
+    return ret
---
+++ 3.graph.cus/start.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=app.py
+
+flask run
+
